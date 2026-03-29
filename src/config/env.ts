329d3e2d715a93e95f06ee90e4c407455ac10e53
priyajan-collab <aswongamikha@gmail.com>
2026-03-29 17:18:14 +0545

    chores:changed localhost API

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ const USE_FASTAPI = true;
 
 const getBaseUrl = (): string => {
   if (USE_FASTAPI) {
-    return "https://candi-unrepaired-nella.ngrok-free.dev/api/v1";
+    return "http://192.168.1.83:8000/api/v1";
   }
   return "http://192.168.1.83:5000/api";
 };
@@ -12,6 +12,6 @@ const getBaseUrl = (): string => {
 export const ENV = {
   API_BASE_URL: getBaseUrl(),
   USE_FASTAPI,
-  PIXABAY_API_BASE_URL: "https://pixabay.com/api/music/",
+  PIXABAY_API_BASE_URL: "https://pixabay.com/api/audio/",
   PIXABAY_API_KEY: "55223370-5892e510692400ad458bc4e62",
 };
